6ae3fcbda5bdfff07386b4167d441f8cca9c81ae
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-11-05 12:12:57 +0100

    First commit

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+let webpack = require('webpack')
+
+module.exports = {
+
+  /*css: {
+    loaderOptions: {
+      sass: {}
+    }
+  },*/
+
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'APP_NAME'   : JSON.stringify(process.env.APP_NAME),
+          'PORT'       : JSON.stringify(process.env.PORT),
+          'PUBLIC_PATH': JSON.stringify(process.env.PUBLIC_PATH),
+        }
+      })
+    ]
+  }
+  ,
+
+// publicPath: process.env.PUBLIC_PATH || '/',
+
+  devServer: {
+    port: process.env.PORT || 4000
+  }
+}
+;
